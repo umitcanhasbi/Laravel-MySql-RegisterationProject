601a85592cc715ca315b2da89b14d83fde8667bc
umitcanhasbi <umitcanhasbioglu@gmail.com>
2017-07-04 12:02:58 +0300

    Update welcome.blade.php

--- Project/Laravel/resources/views/welcome.blade.php
+++ Project/Laravel/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Recep Tayyip Erdrone</title>
+        <title>Registeration</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -89,7 +89,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CcC
+                    Welcome to registeration :)
                 </div>
 
                 <div class="links" style="margin-bottom:25px">
